f5caddb5ebcfe7cec04b4abf6df65a312b266a66
32817 <328170849@qq.com>
2022-07-28 15:48:34 +0800

    Initial commit

---
+++ src/main/java/com/leozhang/portalssm/controller/SexController.java
@@ -0,0 +1,12 @@
+package com.leozhang.portalssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/sex")
+public class SexController {
+
+    
+}
