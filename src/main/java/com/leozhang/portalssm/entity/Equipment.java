788a3281e7b0c929c3cef8f2bfc414ba3792f276
32817 <328170849@qq.com>
2022-08-01 19:04:22 +0800

    增加了设备管理，设备状态管理，设备品牌管理

---
+++ src/main/java/com/leozhang/portalssm/entity/Equipment.java
@@ -0,0 +1,136 @@
+package com.leozhang.portalssm.entity;
+
+import java.util.Date;
+
+public class Equipment {
+    private Long id;
+
+    private String name;
+
+    private Long brandId;
+
+    private String equipmentNo;
+
+    private String remark;
+
+    private Date insertTime;
+
+    private String description;
+
+    private Long statusId;
+
+    private String img;
+
+    private Long roomId;
+
+    private String factory;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Long getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Long brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getEquipmentNo() {
+        return equipmentNo;
+    }
+
+    public void setEquipmentNo(String equipmentNo) {
+        this.equipmentNo = equipmentNo == null ? null : equipmentNo.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Date getInsertTime() {
+        return insertTime;
+    }
+
+    public void setInsertTime(Date insertTime) {
+        this.insertTime = insertTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+
+    public Long getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img == null ? null : img.trim();
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getFactory() {
+        return factory;
+    }
+
+    public void setFactory(String factory) {
+        this.factory = factory == null ? null : factory.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", brandId=").append(brandId);
+        sb.append(", equipmentNo=").append(equipmentNo);
+        sb.append(", remark=").append(remark);
+        sb.append(", insertTime=").append(insertTime);
+        sb.append(", description=").append(description);
+        sb.append(", statusId=").append(statusId);
+        sb.append(", img=").append(img);
+        sb.append(", roomId=").append(roomId);
+        sb.append(", factory=").append(factory);
+        sb.append("]");
+        return sb.toString();
+    }
+}
